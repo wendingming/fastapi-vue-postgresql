131746227abdfbbde5a3cefe73d460a4dd62ee03
wendingming <wendingming@126.com>
2022-02-24 11:01:29 +0800

    编辑管理员

--- vue-fastapi-demo1/src/store/modules/member.js
+++ vue-fastapi-demo1/src/store/modules/member.js
@@ -9,7 +9,7 @@ const user = {
         tokentype: '',
         uid:'',                 //管理员id
         username: '',           //管理员名
-        fullname: '',           //管理员全名
+        full_name: '',           //管理员全名
         avatar: '',             //管理员头像
         email:'',               //管理员邮箱
         permisson:[]            //其它备用【例如：权限】
@@ -27,8 +27,8 @@ const user = {
         SET_USERNAME: (state, username) => {
             state.username = username
         },
-        SET_FULLNAME: (state, fullname) => {
-            state.fullname = fullname
+        SET_FULLNAME: (state, full_name) => {
+            state.full_name = full_name
         },
         SET_AVATAR: (state, avatar) => {
             state.avatar = avatar
@@ -44,12 +44,13 @@ const user = {
         GetInfo({ commit, state }) {//获取管理员信息
             return new Promise((resolve, reject) => {
                 api.getInfo().then(res => {
-                    const user = JSON.parse(JSON.stringify(res));               //绑定管理员信息到常量
+                    const user = JSON.parse(JSON.stringify(res)); //绑定管理员信息到常量
                     const avatar = user.avatar == null ? require("@/assets/img/empty-face.png") : user.avatar;//解析头像地址，没有头像则绑定一张默认头像
-                    commit('SET_USERNAME', user.username)          //绑定姓名
-                    commit('SET_AVATAR', avatar)                  //绑定头像
                     commit('SET_UID',user.uid);                     //绑定id
-                    commit('SET_EMAIL',user.email);           //绑定email
+                    commit('SET_USERNAME', user.username)          //绑定姓名
+                    commit('SET_FULLNAME', user.full_name)          //绑定全名
+                    commit('SET_AVATAR', avatar)                  //绑定头像
+                    commit('SET_EMAIL',user.email);               //绑定email
                     commit('SET_PERMISSON',user.permisson);       //绑定权限
                     resolve(state)
                 }).catch(error => {
@@ -58,7 +59,6 @@ const user = {
             })
         },
         Login({commit},userInfo){//访问登录接口
-            console.log('开始登录');
             return new Promise((resolve,reject)=>{
                 api.login(userInfo).then(res=>{
                     console.log(res);
--- vue-fastapi-demo1/src/views/About.vue
+++ vue-fastapi-demo1/src/views/About.vue
@@ -1,19 +1,134 @@
 <template>
   <div class="about">
     <h1>管理员信息</h1>
-    {{userInfo}}
+    <div class="all-container">
+      <div class="all-container-padding bg" >
+        <el-tabs v-model="activeName">
+          <el-tab-pane label="基本信息" name="first">
+            <el-form :model="userInfo" :rules="rules" ref="EditorUserForms">
+              <el-form-item label="头像" prop="avatar" :label-width="formLabelWidth">
+                <el-upload class="avatar-uploader" action="//shujiaoke.oss-cn-hangzhou.aliyuncs.com" :before-upload="beforeupload" :data="uploadParm" :show-file-list="false" :on-success="handleUpSuccess">
+                  <img v-if="userInfo.avatar" :src="userInfo.avatar" class="avatar">
+                  <i v-else class="el-icon-plus avatar-uploader-icon " style="width:80px;height:80px;"></i>
+                </el-upload>
+              </el-form-item>
+              <el-form-item label="用户名" prop="username" :label-width="formLabelWidth">
+                <el-col :span="8"> <el-input v-model="userInfo.username" disabled ></el-input></el-col>
+              </el-form-item>
+              <el-form-item label="全名" prop="fullname" :label-width="formLabelWidth">
+                <el-col :span="8">  <el-input v-model="userInfo.full_name" placeholder="请输入全名"></el-input></el-col>
+              </el-form-item>
+              <el-form-item label="邮箱" prop="email" :label-width="formLabelWidth">
+                <el-col :span="8">  <el-input v-model="userInfo.email" placeholder="请输入邮箱"></el-input></el-col>
+              </el-form-item>
+              <el-form-item label="用户菜单" prop="permisson" :label-width="formLabelWidth">
+                <el-col :span="8">
+                  <el-input v-model="userInfo.permisson" disabled ></el-input>
+                </el-col>
+              </el-form-item>
+            </el-form>
+            <div class="grid-content bg-purple">
+              <el-button type="primary" @click="EditorUserClick('userInfo')" >保存</el-button>
+            </div>
+          </el-tab-pane>
+          <el-tab-pane label="修改密码" name="second">
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+              <el-form-item label="原密码" prop="pass" :label-width="formLabelWidth">
+                <el-col :span="8">  <el-input v-model="ruleForm.pass" placeholder="请输入原密码" type="password"></el-input></el-col>
+              </el-form-item>
+              <el-form-item label="新密码" prop="newpass" :label-width="formLabelWidth">
+                <el-col :span="8"><el-input v-model="ruleForm.newpass" placeholder="请输入新密码" id="newkey" type="password"></el-input></el-col>
+              </el-form-item>
+              <el-form-item label="重复新密码" prop="checknewpass" :label-width="formLabelWidth">
+                <el-col :span="8">  <el-input v-model="ruleForm.checknewpass" placeholder="请再次输入新密码" id='newkey1' type="password"></el-input></el-col>
+              </el-form-item>
+            </el-form>
+            <div class="grid-content bg-purple">
+              <el-button type="primary" @click="submitForm('ruleForm')">保存</el-button>
+            </div>
+          </el-tab-pane>
+        </el-tabs>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
     //挂载api.js组件
     //import Api from '@/api/api.js'
 
     export default {
         data() {//定义一个变量
+            var validatePass = (rule, value, callback) => {
+                if (value === "") {
+                    callback(new Error("请输入密码"));
+                } else {
+                    if (this.ruleForm.checknewpass !== "") {
+                        this.$refs.ruleForm.validateField("checknewpass");
+                    }
+                    callback();
+                }
+            };
+            var validatePass2 = (rule, value, callback) => {
+                if (value === "") {
+                    callback(new Error("请再次输入密码"));
+                } else if (value !== this.ruleForm.newpass) {
+                    callback(new Error("两次输入密码不一致!"));
+                } else {
+                    callback();
+                }
+            };
             return {
-                userInfo: []
+                userInfo: [],//用户信息表单
+                //uploadParm: {}, //图片的上传
+                ruleForm: {},//修改密码的表单
+                activeName: "first",
+                loading: true,
+                baseUrl: process.env.BASE_API,
+                userlist: {},//用户信息表单
+                formLabelWidth: "150px",
+                /***校验***/
+                rules: {
+                    /*phone: [
+                        {
+                            required: true,
+                            message: "请输入电话号码"
+                        },
+                        {
+                            pattern: /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/,
+                            message: "手机格式不对"
+                        }
+                    ],*/
+                    email: [
+                        {
+                            required: true,
+                            message: "请输入电子邮箱"
+                        },
+                        {
+                            pattern: /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/,
+                            message: "请输入有效的邮箱"
+                        }
+                    ],
+                    pass: [
+                        {
+                            required: true,
+                            trigger: "blur",
+                            message: "请输入密码"
+                        }
+                    ],
+                    newpass: [
+                        {
+                            validator: validatePass,
+                            trigger: "blur"
+                        }
+                    ],
+                    checknewpass: [
+                        {
+                            validator: validatePass2,
+                            trigger: "blur"
+                        }
+                    ]
+                }
             };
         },
         mounted()
@@ -31,6 +146,26 @@
         },
         name: "管理员信息",
         methods:{
+            //修改密码
+            submitForm(ruleForm) {
+                let username = this.userInfo.username;
+                let oldpwd = ruleForm.pass;
+                let newpwd = ruleForm.newpass;
+                var obj = {
+                    username: username,
+                    oldpwd: oldpwd,
+                    newpwd: newpwd
+                };
+                console.log(obj);
+            },
+            // 编辑提交的方法
+            EditorUserClick() {
+                this.$refs.EditorUserForms.validate(valid => {
+                    if (valid) {
+                        console.log(this.userlist);
+                    }
+                });
+            }
         }
     };
 </script>
